d6e19a68f54d93b3f6179f02530447f4865d4839
leonardo1710 <58170984+leonardo1710@users.noreply.github.com>
2026-01-04 17:24:23 +0100

    base

--- src/main/java/at/ac/hcw/App.java
+++ src/main/java/at/ac/hcw/App.java
@@ -1,8 +1,6 @@
 package at.ac.hcw;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-    int funB(){}
+    int count = 0;
+    int inc(){ count++;}
 }
